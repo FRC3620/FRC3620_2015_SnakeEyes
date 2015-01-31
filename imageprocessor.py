c96886cb061a6d9472d70fd6edd6933db1e5da79
Jonathan Peugh <jonathan.peugh44@gmail.com>
2015-01-31 21:33:57 +0000

    Add a little circle to the output so we can see where the middle is.

--- imageprocessor.py
+++ imageprocessor.py
@@ -25,11 +25,14 @@ def process_image( img, debugname, showwindows ):
     dilation = cv2.dilate(open,kernel5,iterations = 3)
     
     close =cv2.morphologyEx(open,cv2.MORPH_CLOSE,kernel20)
+
+    # pick which image you want to do contours on (final, close, whatever)
+    final = close
     
-    #contours morphs source image so we need to use a copy of the image that we are using to get the countour from before using contours
-    test = close.copy()
-    # this seems to screw up the input image
-    contours, hierarchy = cv2.findContours(test,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    # contours morphs source image so we need to use a copy of the image that we are using to get the countour from before using contours
+    contour_image = final.copy()
+    
+    contours, hierarchy = cv2.findContours(contour_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	
     c = contours[0]
     print len(c)
@@ -44,13 +47,15 @@ def process_image( img, debugname, showwindows ):
     print "Center of Mass = (",xc, "," , yc,")"
     
     if showwindows:
-        #cv2.circle(img, (328,265),10,(0,255,0))
-    	cv2.imshow('mask',mask)
+        cv2.circle(img, (int(xc),int(yc)),10,(0,255,0))
     	cv2.imshow('img',img)
     	cv2.imshow('res',res) 
     	cv2.imshow('open',open)
     	cv2.imshow('close',close)
-    	cv2.imshow('deliate',dilation)
+    	cv2.imshow('dilation',dilation)
+    	final = cv2.cvtColor(final, cv2.COLOR_GRAY2BGR)
+        cv2.circle(final, (int(xc),int(yc)),10,(0,255,0))
+    	cv2.imshow('final',final)
 
     print "bye"
     return
