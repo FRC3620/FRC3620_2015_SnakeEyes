efeea0a84d7a66bc12c222b164f94edb42971154
Jonathan Peugh <jonathan.peugh44@gmail.com>
2015-01-31 18:25:05 +0000

    finish with the dilations to remove the FedEx logo from box

--- imageprocessor.py
+++ imageprocessor.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-
+print"some"
 def process_image( img, debugname, showwindows ):
-
+    print"start"
     # Convert BGR to HSV
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -15,10 +15,16 @@ def process_image( img, debugname, showwindows ):
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(img,img, mask= mask)
-
+    
+    kernel = np.ones((5,5),np.uint8)
+    open =cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
+    dilation = cv2.dilate(open,kernel,iterations = 3)
+   
     if showwindows:
     	cv2.imshow('img',img)
-    	cv2.imshow('mask',mask)
+    	#cv2.imshow('mask',mask)
     	cv2.imshow('res',res) 
+    	cv2.imshow('dil',dilation)
+
     return
   
