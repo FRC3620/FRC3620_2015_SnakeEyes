82a8e3fcefae4ddb6e6d7a3b321e67a4d3172703
Jonathan Peugh <jonathan.peugh44@gmail.com>
2015-02-03 01:24:56 +0000

    added logic for aspect ratio at 5% error from long side, short side, and corner.

--- processfileimage.py
+++ processfileimage.py
@@ -1,8 +1,11 @@
 import cv2
 import imageprocessor
 
-im = cv2.imread('tote.jpg')
-imageprocessor.process_image( im, 'dummy', 1 )
+#im = cv2.imread('acquire_14.jpg')
+#im = cv2.imread('tote.jpg')
+#im = cv2.imread('3620s.jpg')
+im = cv2.imread('corner.jpg')
+imageprocessor.process_image( im, 'dummy', 0 )
 
 k = cv2.waitKey()
 
