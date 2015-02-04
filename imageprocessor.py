e82457fe6751418c2d91fe718ba905e8600553e4
Jonathan Peugh <jonathan.peugh44@gmail.com>
2015-02-04 01:22:11 +0000

    added output

--- imageprocessor.py
+++ imageprocessor.py
@@ -3,7 +3,13 @@ import numpy as np
 
 
 def process_image( img, debugname, showwindows ):
-
+    bestScore = -1
+    bestblob = -1
+    current = 0
+    seen = False
+    x= None
+    y= None
+    w= None
     # Convert BGR to HSV
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -42,13 +48,9 @@ def process_image( img, debugname, showwindows ):
          #the diagonal
          corner = 1.83068783069
          
-         bestScore = -1
-         bestblob = -1
-         current = 0
-         for i in range(0, len(contours)):
-             
-             cnt = contours[i]
-             
+         
+         for i in range(0, len(contours)-1):
+
              c = contours[i]
 
              
@@ -58,32 +60,31 @@ def process_image( img, debugname, showwindows ):
              yc = moments['m01'] /moments ['m00']
              xc = moments['m10'] /moments ['m00']
              if area < 11000:
-                 c = None
                  continue
              print i
              print "Contour length = %d" % (len(c))
              print "Area = %d" % (area)
              print "Center of Mass = %d,%d" % (xc, yc)
              
-             x,y,w,h = cv2.boundingRect(cnt)
+             x,y,w,h = cv2.boundingRect(c)
              ratio = float(w)/h
              
              if ratio >= fedEx - .062 and ratio <= fedEx + .062:
                  print "fedEx, ",i
                  cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),i+2)
-                 cv2.putText(img,str(i),(x,y+h),cv2.FONT_HERSHEY_SIMPLEX,.5, (0,0,0),2)
+                 cv2.putText(img,str(i),(x+w/2,y+h/2),cv2.FONT_HERSHEY_SIMPLEX,.5, (0,0,0),2)
                  current = 2*ratio + int(area/1000)
                  
              elif ratio >= team - .2 and ratio <= team + .2:
                  print "team, ",i
                  cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),i+2)
                  
-                 cv2.putText(img,str(i),(x,y+h),cv2.FONT_HERSHEY_SIMPLEX,.5, (0,0,0),2)
+                 cv2.putText(img,str(i),(x+w/2,y+h/2),cv2.FONT_HERSHEY_SIMPLEX,.5, (0,0,0),2)
                  current = 2*ratio + int(area/1000)
              elif ratio >= corner - .9 and ratio <= corner + .9:
                  print "corner, ",i
                  cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),i+2) 
-                 cv2.putText(img,str(i),(x,y+h),cv2.FONT_HERSHEY_SIMPLEX,.5, (0,0,0),2)     
+                 cv2.putText(img,str(i),(x+w/2,y+h/2),cv2.FONT_HERSHEY_SIMPLEX,.5, (0,0,0),2)
                  current = 1.5*ratio + int(area/1000)
              else:
                  cv2.rectangle(img,(x,y), (x+w,y+h),(255,255,255),9)
@@ -94,6 +95,9 @@ def process_image( img, debugname, showwindows ):
              if current > bestScore:
                  bestScore = current
                  bestblob = i
+                 seen = True
+                 bestxc = xc
+                 bestyc = yc
              print "blob ",i," is scored ",current
              print "the best score is ", bestScore," on blob ", bestblob
              
@@ -102,18 +106,23 @@ def process_image( img, debugname, showwindows ):
 
     
     if showwindows:
+        cv2.putText(img,str(bestblob),(int(bestxc),int(bestyc)),cv2.FONT_HERSHEY_SIMPLEX,.5, (0,255,255),2)
     	cv2.imshow('img',img)
     	#cv2.imshow('res',res) 
     	#cv2.imshow('open',open)
     	#cv2.imshow('close',close)
         final = cv2.cvtColor(final, cv2.COLOR_GRAY2BGR)
         if len(contours)!=0:
-            cv2.circle(final, (int(xc),int(yc)),10,(0,255,0))
+            cv2.circle(final, (int(bestxc),int(bestyc)),10,(0,255,0))
             cv2.drawContours (final, contours, -1, (255, 0, 0), 3)
+            cv2.drawContours (final, [contours[bestblob]], -1, (255, 255, 0), 3)
             cv2.imshow('final',final)
         #print hierarchy[0][1]
         #print hierarchy
         #print moments
-
-    return
+        
+    height, width, depth = img.shape
+    if seen:
+        return { 'ImageX': width, 'ImageY':height, 'X':bestxc, 'Y':bestyc , 'Seen':seen}
+    return {'Seen':false}
  
--- processfileimage.py
+++ processfileimage.py
@@ -1,12 +1,12 @@
 import cv2
 import imageprocessor
 
-#im = cv2.imread('acquire_14.jpg')
+im = cv2.imread('acquire_14.jpg')
 #im = cv2.imread('tote.jpg')
 #im = cv2.imread('3620s.jpg')
-im = cv2.imread('corner.jpg')
-imageprocessor.process_image( im, 'dummy', 0 )
-
+#im = cv2.imread('corner.jpg')
+output=imageprocessor.process_image( im, 'dummy', 1 )
+print output
 k = cv2.waitKey()
 
 cv2.destroyAllWindows()
