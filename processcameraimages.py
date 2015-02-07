c7a3b8fc3fbfd9c1e3f81dc377c27deb9724ef18
Jonathan Peugh <jonathan.peugh44@gmail.com>
2015-02-07 19:45:34 +0000

    added settings that allow the retrieval of hsv values from a file

--- processcameraimages.py
+++ processcameraimages.py
@@ -3,7 +3,7 @@ import cv2
 import imageprocessor
 import socket
 import json
-    
+
 UDP_IP = "roborio-3620.local"
 UDP_PORT = 3620
 
@@ -21,15 +21,20 @@ cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
 #cap.set(cv2.cv.CV_CAP_PROP_FPS, 2)
 
+start = open('videoSettings.json','r')
+sread = start.read()
+initialHSV=json.loads(sread)
+start.close()
+
 while(1):
 
     # Take each frame
     _, frame = cap.read()
     
-    #cv2.imshow('frame',frame)
+    cv2.imshow('frame',frame)
     
     #get information aboot the image
-    output=imageprocessor.process_image (frame, 'dummy', 0)
+    output, images=imageprocessor.process_image (frame,initialHSV, markup=0 )
     
     #convert to JSON
     jsontote = json.dumps(output)
@@ -38,12 +43,13 @@ while(1):
     if sock is not None:
         #send to network
         try:
-            sock.sendto(jsontote, (UDP_IP, UDP_PORT))
+            pass
+            #sock.sendto(jsontote, (UDP_IP, UDP_PORT))
         except Exception as foo:
     	    print type(foo)
 	    print "Huh, had trouble making the socket"
     
-    #k = cv2.waitKey(5) & 0xFF
+    k = cv2.waitKey(5) & 0xFF
 
 #clean up
 cv2.destroyAllWindows() 
---
+++ processfileimagesliders.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python2
+
+import cv2
+import imageprocessor
+import numpy as np
+import Tkinter
+import argparse
+import json
+import time
+#initialHSV={'vLo': 100, 'hLo': 20, 'sLo': 100, 'vHi': 255, 'sHi': 255, 'hHi': 50}
+#hsvd = initialHSV
+currentHSV={}
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+args = parser.parse_args()
+
+print "Processing file %s", args.file
+
+im = cv2.imread(args.file)
+
+def nothing(x):
+    pass
+def writeFile(s):
+    if s == 1:
+        print 'Saving...'
+        
+        with open('videoSettings.json','w') as outfile:
+            print currentHSV
+            json.dump(currentHSV, outfile)
+        print "...Done"
+    cv2.setTrackbarPos('Save','image',0)
+    
+def reset(r):
+    if r==1:
+        cv2.setTrackbarPos('hLo','image',initialHSV.get('hLo'))
+        cv2.setTrackbarPos('hHi','image',initialHSV.get('hHi'))
+        cv2.setTrackbarPos('sLo','image',initialHSV.get('sLo'))
+        cv2.setTrackbarPos('sHi','image',initialHSV.get('sHi'))
+        cv2.setTrackbarPos('vLo','image',initialHSV.get('vLo'))
+        cv2.setTrackbarPos('vHi','image',initialHSV.get('vHi'))
+        writeFile(1)
+    cv2.setTrackbarPos('Reset and Save','image',0)
+def process(o):
+    print o
+
+    # get current positions of six trackbars
+    hLo = cv2.getTrackbarPos('hLo','image')
+    hHi = cv2.getTrackbarPos('hHi','image')
+    sLo = cv2.getTrackbarPos('sLo','image')
+    sHi = cv2.getTrackbarPos('sHi','image')
+    vLo = cv2.getTrackbarPos('vLo','image')
+    vHi = cv2.getTrackbarPos('vHi','image')
+    global currentHSV
+    currentHSV = {'hLo': hLo, 'hHi': hHi, 'sLo': sLo, 'sHi': sHi, 'vLo': vLo, 'vHi': vHi}
+    print currentHSV
+    
+    # get imformation about the image
+    output, images = imageprocessor.process_image( im, currentHSV, markup=1 )
+    print output
+    cv2.imshow('image',im)
+
+    for n, img in images.iteritems():
+	cv2.imshow(n, img)
+
+
+# Create a black image, a window
+cv2.namedWindow('image')
+
+start = open('videoSettings.json','r')
+sread = start.read()
+initialHSV=json.loads(sread)
+currentHSV= initialHSV.copy()
+start.close()
+
+
+# create trackbars for color change
+cv2.createTrackbar('hLo','image',initialHSV.get('hLo'),255,process)
+cv2.createTrackbar('hHi','image',initialHSV.get('hHi'),255,process)
+cv2.createTrackbar('sLo','image',initialHSV.get('sLo'),255,process)
+cv2.createTrackbar('sHi','image',initialHSV.get('sHi'),255,process)
+cv2.createTrackbar('vLo','image',initialHSV.get('vLo'),255,process)
+cv2.createTrackbar('vHi','image',initialHSV.get('vHi'),255,process)
+cv2.createTrackbar('Save','image',0,1,writeFile)
+cv2.createTrackbar('Reset and Save','image',0,1,reset)
+
+process(0)
+
+
+while(1):
+
+    k = cv2.waitKey(200) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
+
