a2c30dbcfb8b15f23070c6de3188cd750d0e9ad5
Doug Wegscheid <dwegscheid@sbcglobal.net>
2015-02-28 11:29:43 -0500

    Added code to let the robot change what file to save video to and updated sendData to change file every 30 seconds

--- recorder.py
+++ recorder.py
@@ -3,12 +3,21 @@ import cv2
 import time
 import socket
 import errno
+import json
 
 start = time.time()
 UDP_IP = "0.0.0.0"
 UDP_PORT = 3620
 fail = 0
 sock = None
+y_offset = 0
+x_offset = 0
+oldfile = None
+filename = None
+tFromLast = 0
+out = None
+
+
 print 'making socket connection'
 try:
     sock = socket.socket(socket.AF_INET, # Internet
@@ -19,54 +28,67 @@ except Exception as foo:
     print type(foo)
     print "Huh, had trouble making the socket"
 print 'done making socket connection'
+##############################################################################
+#When can we get a compotent text editor
 cap = cv2.VideoCapture(0)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320)
 cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
 fourcc = cv2.cv.CV_FOURCC(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 17, (320,260))
-
-
-y_offset = 0
-x_offset = 0
-count = 0
-
+##############################################################################
 while(cap.isOpened()):
-    
-    img1 = np.zeros((260,320, 3), np.uint8)
-    ret, frame = cap.read()
-    if sock is not None:
-        #send to network
-        try:
-            
-            data, addr = sock.recvfrom(256) # buffer size is 256 bytes
-        except socket.error, e:
-            err = e.args[0]
-            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
-                data="No socket @ t=", time.time()-start
-                print data
-            else:
-                print e
-        else:
-            print data
-    if ret == True:
-        img2 = frame
-        h,w,d = img2.shape
-        img1[y_offset:y_offset+h, x_offset:x_offset+w] = img2
-        cv2.putText(img1,(str(data) +' '+str(time.time()-start)),(0+3,250),2,.35,(255,255,255))
-        cv2.imshow('res', img1)
-         
-        out.write(img1)
-        
-        if cv2.waitKey(1)& 0xFF == ord('q'):
-             break
-        #if time.time()-start > 1:
-             #break
-        #count = count +1
-    else:
-        break
 
+    #Reading Data
+    try:
+        data, addr = sock.recvfrom(256) # buffer size is 256 by
+        data= json.loads(data)
+        stringToPrint = "{time} match, {tim} sec, team {team}, {volt} volts".format(time=data['time'],tim=data['timeinmatch'],team=data['position'],volt=data['voltage'])
+        tFromLast = time.time()
+        filename = data['filename']
+    except socket.error, e:
+        err = e.args[0]
+        if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+            if time.time()-tFromLast > 10:
+                stringToPrint="Been a long time sense home called..."
+                if filename is not None:
+                    print stringToPrint
+                filename = None
+        else:
+            print e
+            
+    #Changing files 
+    #DEBUG print "filename {}, oldfile {}, out {}".format(filename,oldfile,out)    
+    if filename != oldfile:
+        if out is not None:
+            out.release()
+            print 'file closing'
+        out = None
+        
+    oldfile = filename
+    
+    #We don't have a file open but we want one
+    if filename is not None and out is None:
+        out = cv2.VideoWriter(filename,fourcc,17,(320,260))
+        print "file changed to {}".format(filename)
+
+        
+    if out is not None:
+        ret, frame = cap.read()
+        
+        if ret == True: 
+            img1 = np.zeros((260,320, 3), np.uint8)
+            img2 = frame
+            h,w,d = img2.shape
+            img1[y_offset:y_offset+h, x_offset:x_offset+w] = img2
+            cv2.putText(img1,stringToPrint,(0+3,250),2,.35,(255,255,255))
+            cv2.imshow('res', img1)
+            out.write(img1)
+    if cv2.waitKey(1)& 0xFF == ord('q'):
+       break
+            
 cap.release()
-out.release()
-#print time.time() - start
-#print count
+if out is not None:
+    out.release()
+
+###################################################################################
+
 cv2.destroyAllWindows()
--- sendData.py
+++ sendData.py
@@ -1,9 +1,23 @@
 import numpy as np
 import cv2
 import socket
+import time as t
+import json
+
+start = t.time()
+print start
 
 UDP_IP = "localhost"
 UDP_PORT = 3620
+
+filename = 'test.avi'
+robotmode = 'teleop'
+time = '10:42'
+
+joystickx = 0
+joysticky = 0
+position = 'Red 3'
+voltage = '12.42'
 
 sock = None
 
@@ -16,9 +30,19 @@ except Exception as foo:
 
 
 while True:
+    timeinmatch = str(int(t.time()-start))
+    data = json.dumps({'filename':filename,'robotmode':robotmode,'time':time,'timeinmatch':timeinmatch,'joystickx':joystickx,'joysticky':joysticky,'position':position,'voltage':voltage})
     if sock is not None:
         try:
-            sock.sendto('howdy',(UDP_IP, UDP_PORT))
+            sock.sendto(data,(UDP_IP, UDP_PORT))
+            print timeinmatch
+            t.sleep(.05)
         except Exception as foo:
     	    print type(foo)
 	    print "Huh, had trouble making the socket"
+    if int(t.time()-start) == 30:
+        filename = 'testChange.avi'
+        print filename
+    if int(t.time()-start) == 60:
+        start = t.time()
+        print filename
